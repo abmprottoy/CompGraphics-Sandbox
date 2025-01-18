068eaac0927c3353d2d5b2356054fa9c23828ede
ABM Prottoy <abmlabs@outlook.com>
2025-01-18 16:35:55 +0600

    Add AbandonedCity

---
+++ AbandonedCity/main.cpp
@@ -0,0 +1,34 @@
+#include <Windows.h>
+#include <GL/freeglut.h>
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT); // Clear the screen
+
+    // Begin drawing a triangle
+    glBegin(GL_TRIANGLES);
+    // Set the color to red for the first vertex
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex2f(-0.5f, -0.5f);
+
+    // Set the color to green for the second vertex
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex2f(0.5f, -0.5f);
+
+    // Set the color to blue for the third vertex
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex2f(0.0f, 0.5f);
+    glEnd(); // End drawing the triangle
+
+    glFlush(); // Render now
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(1024, 576);
+	glutInitWindowPosition(300, 300);
+    glutCreateWindow("Graphics Introduction");
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;
+}
